0ab6492085bfb404f4a2145a251034b06c781592
CoderforTest <cyclist582@gmail.com>
2019-12-06 19:49:01 -0500

    'hiiiii'

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -2,5 +2,6 @@ public class SecondClass {
 
     public static void main(String[] args) {
         System.out.println("This is the second class.");
+        System.out.println("Second class change");
     }
 }
