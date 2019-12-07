47ee408a9a263b52bf1d34f0b5ea9f72ba6d95d3
CoderforTest <55067239+CoderforTest@users.noreply.github.com>
2019-12-06 19:54:25 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,5 +3,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("This is the second class.");
         System.out.println("Second class change");
+        System.out.println("Change in remote");
     }
 }
