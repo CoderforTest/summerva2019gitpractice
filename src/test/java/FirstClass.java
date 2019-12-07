df2a738591873cae04cc6466e1776c7cfdb0109b
CoderforTest <cyclist582@gmail.com>
2019-12-06 20:10:52 -0500

    'hi'

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,6 +3,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hi world. stash");
         System.out.println("I am working.");
-       System.out.println("Change1");
+       System.out.println("Stash");
     }
 }
--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,7 @@
 public class SecondClass {
 
     public static void main(String[] args) {
-        System.out.println("This is the second class.");
+        System.out.println("This is the second class. Yes.");
         System.out.println("Second class change");
         System.out.println("Change in remote");
     }
